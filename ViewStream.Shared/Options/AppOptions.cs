0985d16b9478bb7c004cbd0d2856d54d213101dd
ِPro-Khaled <Pro.Khaled56@gmail.com>
2026-04-11 01:35:24 +0300

    Add App options

---
+++ ViewStream.Shared/Options/AppOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewStream.Shared.Options
+{
+    public class AppOptions
+    {
+        public const string SectionName = "App";
+        public string BaseUrl { get; set; } = "https://localhost:5001"; // default
+    }
+}
