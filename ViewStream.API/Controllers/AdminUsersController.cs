1dcd030a2e40b91614fc4fcd5a11ef4d7bc848ce
ِPro-Khaled <Pro.Khaled56@gmail.com>
2026-05-10 04:38:59 +0300

    remove files that i not used

--- ViewStream.API/Controllers/AdminUsersController.cs
+++ ViewStream.API/Controllers/AdminUsersController.cs
@@ -27,30 +27,6 @@ public class AdminUsersController : ControllerBase
 
     #region Queries
 
-    /// <summary>
-    /// Retrieves a paginated list of all users.
-    /// </summary>
-    /// <param name="page">Page number (1‑indexed).</param>
-    /// <param name="pageSize">Number of items per page.</param>
-    /// <param name="search">Optional search for email or name.</param>
-    /// <param name="cancellationToken">Cancellation token.</param>
-    /// <returns>A paginated list of users.</returns>
-    /// <response code="200">Returns the paginated list of users.</response>
-    /// <response code="401">User is not authenticated.</response>
-    /// <response code="403">User does not have permission.</response>
-    [HttpGet]
-    [ProducesResponseType(typeof(PagedResult<UserDto>), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    public async Task<ActionResult<PagedResult<UserDto>>> GetUsers(
-        [FromQuery] int page = 1,
-        [FromQuery] int pageSize = 20,
-        [FromQuery] string? search = null,
-        CancellationToken cancellationToken = default)
-    {
-        var result = await _mediator.Send(new GetUsersPagedQuery(page, pageSize, search), cancellationToken);
-        return Ok(result);
-    }
 
     /// <summary>
     /// Retrieves a single user by ID.
--- ViewStream.Application/Queries/User/GetUsersPagedQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using ViewStream.Application.Common;
-using ViewStream.Application.DTOs;
-
-namespace ViewStream.Application.Queries.User
-{
-    public record GetUsersPagedQuery(int Page = 1, int PageSize = 20, string? SearchTerm = null)
-        : IRequest<PagedResult<UserDto>>;
-}
